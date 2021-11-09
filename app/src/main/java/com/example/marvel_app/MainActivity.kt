84d3d122012544110b76c809a7521384d5d88ce7
Arthur <arthur.chevauchet@gmail.com>
2021-11-09 10:58:07 +0100

    fragment

--- app/src/main/java/com/example/marvel_app/MainActivity.kt
+++ app/src/main/java/com/example/marvel_app/MainActivity.kt
@@ -1,26 +1,31 @@
 package com.example.marvel_app
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import com.example.marvel_app.model.JsonResponse
 import com.example.marvel_app.model.MarvelCharacter
 import com.example.marvel_app.usecase.GetMarvelCharacterUseCase
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationBarView
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
 import kotlin.math.log
 
 class MainActivity : AppCompatActivity() {
 
     private val scope = CoroutineScope(Dispatchers.IO)
 
+    var bottomNavigationView: BottomNavigationView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initNav()
 
         scope.launch {
             val jsonResponse = Gson().fromJson<JsonResponse<MarvelCharacter>>(
@@ -30,5 +35,14 @@ class MainActivity : AppCompatActivity() {
             Log.d("Main", "onCreate: $jsonResponse")
 
         }
+    }
+
+    private fun initNav() {
+        Log.d("Main", "initNav: start")
+        bottomNavigationView = findViewById(R.id.bottomNavBar)
+        bottomNavigationView?.itemIconTintList = null
+        bottomNavigationView?.labelVisibilityMode = NavigationBarView.LABEL_VISIBILITY_SELECTED
+        bottomNavigationView?.selectedItemId = R.id.characters
+        Log.d("Main", "initNav: end")
     }
 }
